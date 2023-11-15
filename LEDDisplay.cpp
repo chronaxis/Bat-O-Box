593235743d34d527481e83cb4570156915fb4a86
chronaxis <thewrinklycheese@gmail.com>
2023-11-14 23:41:16 -0500

    Initial setup of LEDDisplay Class
    Class to handle the LEDDisplay - printing should be the main function. Need to read up on documentation for the LED display.

---
+++ LEDDisplay.cpp
@@ -0,0 +1,18 @@
+/*
+ * LEDDisplay.cpp
+ *
+ *  Created on: Nov 14, 2023
+ *      Author: Dennis Chen and Tristan Lok
+ */
+
+#include "LEDDisplay.hpp"
+
+LEDDisplay::LEDDisplay() {
+
+}
+
+void LEDDisplay::print() {
+
+}
+
+
---
+++ LEDDisplay.hpp
@@ -0,0 +1,18 @@
+/*
+ * UltrasonicSensor.hpp
+ *
+ *  Created on: Nov 14, 2023
+ *      Author: thewr
+ */
+
+#ifndef INC_LEDDISPLAY_HPP_
+#define INC_LEDDISPLAY_HPP_
+
+class LEDDisplay {
+	private:
+		LEDDisplay();
+	public:
+		void print();
+};
+
+#endif /* INC_LEDDISPLAY_HPP_ */
